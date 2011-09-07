db5a54638abd774ca5ce4631221c500e63f8a1e6
H.Merijn Brand <merijn@lx09.procura.nl>
2011-09-07 08:21:50 +0200

    More cross-checks for META data

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -11,81 +11,34 @@ GetOptions (
     "v|verbose:1"	=> \$opt_v,
     ) or die "usage: $0 [--check]\n";
 
-my $version;
-open my $pm, "<", "lib/DBD/CSV.pm" or die "Cannot read CSV.pm";
-while (<$pm>) {
-    m/^.VERSION\s*=\s*"?([-0-9._]+)"?\s*;\s*$/ or next;
-    $version = $1;
-    last;
-    }
-close $pm;
+use lib "sandbox";
+use genMETA;
+my $meta = genMETA->new (
+    from    => "lib/DBD/CSV.pm",
+    verbose => $opt_v,
+    );
 
-my @yml;
-while (<DATA>) {
-    s/VERSION/$version/o;
-    push @yml, $_;
-    }
+$meta->from_data (<DATA>);
 
 if ($check) {
-    print STDERR "Check required and recommended module versions ...\n";
-    BEGIN { $V::NO_EXIT = $V::NO_EXIT = 1 } require V;
-    my %vsn = map { m/^\s*([\w:]+):\s+([0-9.]+)$/ ? ($1, $2) : () } @yml;
-    delete @vsn{qw( perl version )};
-    for (sort keys %vsn) {
-	$vsn{$_} eq "0" and next;
-	my $v = V::get_version ($_);
-	$v eq $vsn{$_} and next;
-	printf STDERR "%-35s %-6s => %s\n", $_, $vsn{$_}, $v;
-	}
-    if (open my $bh, "<", "lib/Bundle/DBD/CSV.pm") {
-	print STDERR "Check bundle module versions ...\n";
-	while (<$bh>) {
-	    my ($m, $dv) = m/^([A-Za-z_:]+)\s+([0-9.]+)\s*$/ or next;
-	    my $v = $m eq "DBD::CSV" ? $version : V::get_version ($m);
-	    $v eq $dv and next;
-	    printf STDERR "%-35s %-6s => %s\n", $m, $dv, $v;
-	    }
-	}
-
-    print STDERR "Checking generated YAML ...\n";
-    use YAML::Syck;
-    use Test::YAML::Meta::Version;
-    my $h;
-    my $yml = join "", @yml;
-    eval { $h = Load ($yml) };
-    $@ and die "$@\n";
-    $opt_v and print Dump $h;
-    my $t = Test::YAML::Meta::Version->new (yaml => $h);
-    $t->parse () and die join "\n", $t->errors, "";
-
-    use Parse::CPAN::Meta;
-    eval { Parse::CPAN::Meta::Load ($yml) };
-    $@ and die "$@\n";
-
-    my $reqvsn = $h->{requires}{perl};
-    print "Checking if $reqvsn is still OK as minimal version for examples\n";
-    use Test::MinimumVersion;
-    # All other minimum version checks done in xt
-    all_minimum_version_ok ($reqvsn, { paths => [ "t", "lib" ]});
+    $meta->check_encoding ();
+    $meta->check_required ();
+    $meta->check_minimum ([ "t", "lib" ]);
     }
 elsif ($opt_v) {
-    print @yml;
+    $meta->print_yaml ();
     }
 else {
-    my @my = glob <*/META.yml>;
-    @my == 1 && open my $my, ">", $my[0] or die "Cannot update META.yml\n";
-    print $my @yml;
-    close $my;
-    chmod 0644, glob <*/META.yml>;
+    $meta->fix_meta ();
     }
 
 __END__
 --- #YAML:1.0
-name:                    DBD::CSV
+name:                    DBD-CSV
 version:                 VERSION
 abstract:                DBI driver for CSV files
 license:                 perl
-author:              
+author:
     - Jochen Wiedmann
     - Jeff Zucker
     - H.Merijn Brand <h.m.brand@xs4all.nl>
@@ -96,7 +49,7 @@ provides:
     DBD::CSV:
         file:            lib/DBD/CSV.pm
         version:         VERSION
-requires:     
+requires:
     perl:                5.008001
     DBI:                 1.614
     DBD::File:           0.40
@@ -112,7 +65,7 @@ test_requires:
     Encode:              0
     Cwd:                 0
     charnames:           0
-recommends:     
+recommends:
     perl:                5.014001
     DBI:                 1.616
     Text::CSV_XS:        0.83
