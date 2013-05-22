b401d52d1604a28e4981bed60f0b5af2e499d029
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2013-05-22 20:30:54 +0200

    Require SQL::Statement 1.403

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -53,7 +53,7 @@ requires:
     perl:                5.008001
     DBI:                 1.627
     DBD::File:           0.41
-    SQL::Statement:      1.402
+    SQL::Statement:      1.403
     Text::CSV_XS:        0.97
 configure_requires:
     ExtUtils::MakeMaker: 0
