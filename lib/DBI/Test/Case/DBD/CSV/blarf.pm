f219dd916eb2fefd268e86c97bc7bfe83ed2e2b3
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2013-08-13 15:51:38 +0200

    Convert the base test
    mvb is unsupported
    TODO: try using csv_cslls DBD::CSV when available under Pure_Perl

--- lib/DBI/Test/Case/DBD/CSV/blarf.pm
+++ lib/DBI/Test/Case/DBD/CSV/blarf.pm
@@ -7,6 +7,7 @@ use parent qw( DBI::Test::DBD::CSV::Case);
 
 use Test::More;
 use DBI::Test;
+use DBI;
 
 sub supported_variant
 {
@@ -14,6 +15,9 @@ sub supported_variant
 	$dsn_pfx, $dsn_cred,  $options) = @_;
 
     #use DP;DDumper$test_confs;
+    #use DP;DDumper{tc=>$test_case,cp=>$cfg_pfx,dp=>$dsn_pfx,op=>$options};
+    $cfg_pfx =~ m/mvb/ and return;
+
     #scalar grep { $_->{abbrev} eq "g" } @$test_confs and return;
     return $self->SUPER::supported_variant ($test_case, $cfg_pfx, $test_confs,
 	$dsn_pfx, $dsn_cred, $options);
@@ -26,13 +30,41 @@ sub run_test
 
     # note ("Running tests for $test_dbd");
 
+    do "t/lib.pl";
+
+    my $nano = $ENV{DBI_SQL_NANO};
+    unless (defined $nano) {
+	$nano = "not set";
+	eval "use SQL::Statement;";
+	ok ($SQL::Statement::VERSION, "SQL::Statement::Version $SQL::Statement::VERSION");
+	}
+    diag ("Showing relevant versions (DBI_SQL_NANO = $nano)");
+    diag ("Using DBI            version $DBI::VERSION");
+    diag ("Using DBD::File      version $DBD::File::VERSION");
+    diag ("Using SQL::Statement version $SQL::Statement::VERSION");
+    diag ("Using Text::CSV_XS   version $Text::CSV_XS::VERSION");
+
+    ok (my $switch = DBI->internal, "DBI->internal");
+    is (ref $switch, "DBI::dr", "Driver class");
+
+    # This is a special case. install_driver should not normally be used.
+    ok (my $drh = DBI->install_driver ("CSV"), "Install driver");
+
+    is (ref $drh, "DBI::dr", "Driver class installed");
+
+    ok ($drh->{Version}, "Driver version $drh->{Version}");
+
     # Test RaiseError for prepare errors
-    #
     $DB_CREDS[3]->{PrintError} = 0;
     $DB_CREDS[3]->{RaiseError} = 0;
     my $dbh = connect_ok (@DB_CREDS, "Connect with dbi:CSV:");
 
     ok ($dbh, "DBH created");
+
+
+    my $csv_version_info = $dbh->csv_versions ();
+    ok ($csv_version_info, "csv_versions");
+    diag ($csv_version_info);
 
     done_testing ();
     }
