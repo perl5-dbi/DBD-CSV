fbf97ad0a33c9b60dfe2b4bc137e81ed83e9c087
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2015-01-01 13:51:23 +0100

    Updated copyright to 2015

--- lib/DBD/CSV.pm
+++ lib/DBD/CSV.pm
@@ -1196,7 +1196,7 @@ Previous maintainer was Jeff Zucker
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009-2014 by H.Merijn Brand
+Copyright (C) 2009-2015 by H.Merijn Brand
 Copyright (C) 2004-2009 by Jeff Zucker
 Copyright (C) 1998-2004 by Jochen Wiedmann
 
