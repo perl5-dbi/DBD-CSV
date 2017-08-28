44939f9a181ae01324158dca145dabbc51889122
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2017-08-28 17:21:27 +0200

    Add test case for not-failing RT#121127

---
+++ sandbox/rt121127.pl
@@ -0,0 +1,33 @@
+#!/pro/bin/perl
+
+use 5.14.2;
+use warnings;
+
+use DBI;
+use Data::Peek;
+
+binmode STDERR, ":encoding(utf-8)";
+
+my $dir = -d "sandbox" ? "sandbox" : ".";
+
+my $dbh = DBI->connect ("dbi:CSV:", undef, undef, {
+    f_ext		=> ".csv/r",
+    f_dir		=> $dir,
+    csv_null		=> 1,
+    csv_sep_char	=> "|",
+    csv_quote_char	=> undef,
+    });
+
+my $sth = $dbh->prepare (<<";");
+select   *
+from     rt121127
+where    entry IS NOT NULL
+    and  entry NOT LIKE '#%'
+    and  band  LIKE ?
+order by entry
+;
+
+$sth->execute ("Canberra Brass");
+while (my $row = $sth->fetch) {
+    DDumper $row;
+    }
